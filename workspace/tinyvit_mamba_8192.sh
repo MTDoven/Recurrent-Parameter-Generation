92a6460dbfa0d17beb4c555c04e42997893c5a3c
MTDoven <MTDoven@gmail.com>
2024-08-05 07:01:37 +0000

    update dataset log

--- workspace/tinyvit_mamba_8192.sh
+++
@@ -1,9 +0,0 @@
-accelerate launch \
-  --main_process_port=29504 \
-  --multi_gpu \
-  --num_processes=2 \
-  --gpu_ids='0,1' \
-  --num_machines=1 \
-  --mixed_precision=bf16 \
-  --dynamo_backend=no \
-  tinyvit_mamba_8192.py \
