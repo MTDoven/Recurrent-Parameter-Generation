c1072052508e501e955d13a643142fd44d09f393
MTDoven <MTDoven@gmail.com>
2024-08-07 14:16:31 +0000

    fix bugs in vittiny_condition_8192

--- workspace/vittiny_condition_8192.sh
+++ workspace/vittiny_condition_8192.sh
@@ -1,7 +1,8 @@
 accelerate launch \
-  --main_process_port=29508 \
-  --num_processes=1 \
-  --gpu_ids='1' \
+  --main_process_port=29511 \
+  --multi_gpu \
+  --num_processes=2 \
+  --gpu_ids='1,2' \
   --num_machines=1 \
   --mixed_precision=bf16 \
   --dynamo_backend=no \
