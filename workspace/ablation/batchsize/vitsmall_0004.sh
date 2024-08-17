726f5ce031ebaf35ac54418ed2d30d81ed7dcc43
MTDoven <MTDoven@gmail.com>
2024-08-17 04:54:44 +0000

    update batchsize

--- workspace/ablation/batchsize/vitsmall_0004.sh
+++ workspace/ablation/batchsize/vitsmall_0004.sh
@@ -1,7 +1,7 @@
 accelerate launch \
   --main_process_port=29552 \
   --num_processes=1 \
-  --gpu_ids='5' \
+  --gpu_ids='7' \
   --num_machines=1 \
   --mixed_precision=bf16 \
   --dynamo_backend=no \
