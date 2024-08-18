20a6e66f0dfeb3012247864d121ac15f78886fbf
MTDoven <MTDoven@gmail.com>
2024-08-18 14:15:58 +0000

    update convnext198m

--- dataset/imagenet_convnext_198m/model.py
+++ dataset/imagenet_convnext_198m/model.py
@@ -3,5 +3,5 @@ import torch
 from torch import nn
 
 def imagenet_classify():
-    model = timm.create_model('convnext_atto', pretrained=True)
+    model = timm.create_model('convnext_large', pretrained=True)
     return model
