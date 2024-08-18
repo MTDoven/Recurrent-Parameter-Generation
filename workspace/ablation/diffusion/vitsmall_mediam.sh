5e8f999f84af6562a99dc50727de8f3147386554
MTDoven <MTDoven@gmail.com>
2024-08-18 09:49:44 +0000

    update diffusion

---
+++ workspace/ablation/diffusion/vitsmall_mediam.sh
@@ -0,0 +1,8 @@
+accelerate launch \
+  --main_process_port=29534 \
+  --num_processes=1 \
+  --gpu_ids='1' \
+  --num_machines=1 \
+  --mixed_precision=bf16 \
+  --dynamo_backend=no \
+  vitsmall_mediam.py \
