a87e6405f37249c0ee52ad63d50ad7e9dc4b2b9c
MTDoven <MTDovent@gmail.com>
2024-08-19 21:28:41 +0800

    commit test

--- temp.py
+++ temp.py
@@ -14,3 +14,7 @@ for (key1, value1), (key2, value2) in zip(diction1.items(), diction2.items()):
     if key1 != "head.0.bias":
         print(key1, norm(value1.flatten()[:5]), "\n",
               key1, norm(value2.flatten()[:5]))
+
+
+
+
