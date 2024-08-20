b10ee091eae3891f64dc86e84d1000a423055c4a
MTDoven <MTDovent@gmail.com>
2024-08-20 10:08:18 +0800

    delete imagenet1000

--- dataset/imagenet_vittiny_classifier/model.py
+++
@@ -1,8 +0,0 @@
-import timm
-import torch
-from torch import nn
-
-def imagenet_classify():
-    model = timm.create_model('vit_tiny_patch16_224', pretrained=True)
-    model.head = nn.Sequential(nn.Linear(192, 1), nn.Flatten(start_dim=0))
-    return model
