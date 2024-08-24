7a123f304a52baf6cc6a53c7b8a52d2bb51f1d87
MTDoven <MTDoven@gmail.com>
2024-08-24 13:26:45 +0000

    update workspace

--- workspace/main/imagenet_resnet50_base.sh
+++ workspace/main/imagenet_resnet50_base.sh
@@ -1,8 +1,8 @@
 accelerate launch \
-  --main_process_port=29601 \
+  --main_process_port=29602 \
   --num_processes=1 \
   --gpu_ids='7' \
   --num_machines=1 \
-  --mixed_precision=no \
+  --mixed_precision=bf16 \
   --dynamo_backend=no \
-  imagenet_resnet18_base.py \
+  imagenet_resnet50_base.py \
