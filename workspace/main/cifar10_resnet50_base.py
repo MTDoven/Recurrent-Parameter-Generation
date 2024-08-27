f22858191aa496b8b1ef821fdee0264ca6d8bf1c
MTDoven <MTDoven@gmail.com>
2024-08-27 15:52:20 +0000

    update workspace

--- workspace/main/cifar10_resnet50_base.py
+++ workspace/main/cifar10_resnet50_base.py
@@ -34,7 +34,7 @@ config = {
     "dim_per_token": 8192,
     "sequence_length": 'auto',
     # train setting
-    "batch_size": 4,
+    "batch_size": 1,
     "num_workers": 8,
     "total_steps": 50000,
     "learning_rate": 0.00003,
