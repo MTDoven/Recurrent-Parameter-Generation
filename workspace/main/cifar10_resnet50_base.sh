b6a77510fe42e55c2376387e7d3dea74cc2c6783
MTDoven <MTDoven@gmail.com>
2024-08-27 16:08:00 +0000

    update workspace

--- workspace/main/cifar10_resnet50_base.sh
+++ workspace/main/cifar10_resnet50_base.sh
@@ -1,7 +1,7 @@
 accelerate launch \
   --main_process_port=29602 \
   --num_processes=1 \
-  --gpu_ids='1' \
+  --gpu_ids='6' \
   --num_machines=1 \
   --mixed_precision=bf16 \
   --dynamo_backend=no \
