3f7079c3f2cab7105b3214cd6b5bc6f88b7c350c
MTDoven <MTDoven@gmail.com>
2024-09-02 12:53:43 +0000

    update vitbase

--- workspace/ablation/slice_channel/ablation_slice_channel.py
+++ workspace/ablation/slice_channel/ablation_slice_channel.py
@@ -36,7 +36,7 @@ from torch.utils.data import DataLoader
 config = {
     # dataset setting
     "dataset": Dataset,
-    "dim_per_token": 8192,
+    "dim_per_token": 2048,
     "sequence_length": 'auto',
     "num_permutation_state": 'auto',
     # train setting
@@ -58,14 +58,14 @@ config["model_config"] = {
     # mamba config
     "num_permutation_state": config["num_permutation_state"],
     "d_condition": 1,
-    "d_model": 8192,
-    "d_model_1": 8192,
-    "d_model_2": 8192,
+    "d_model": 2048,
+    "d_model_1": 2048,
+    "d_model_2": 2048,
     "d_state": 128,
     "d_conv": 4,
     "expand": 2,
     # diffusion config
-    "diffusion_batch": 512,
+    "diffusion_batch": 2048,
     "layer_channels": [1, 32, 64, 128, 64, 32, 1],
     "dim_per_token": config["dim_per_token"],
     "kernel_size": 7,
