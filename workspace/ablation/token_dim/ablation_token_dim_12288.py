1086327ef40d2c42a6f29a0536bd92c9f8dbaec0
MTDoven <MTDoven@gmail.com>
2024-09-03 03:35:52 +0000

    update

--- workspace/ablation/token_dim/ablation_token_dim_12288.py
+++ workspace/ablation/token_dim/ablation_token_dim_12288.py
@@ -7,7 +7,7 @@ USE_WANDB = True
 import random
 import numpy as np
 import torch
-seed = SEED = 20040424
+seed = SEED = 20040421
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
@@ -43,7 +43,7 @@ config = {
     "batch_size": 4,
     "num_workers": 8,
     "total_steps": 80000,
-    "learning_rate": 0.00005,
+    "learning_rate": 0.00003,
     "weight_decay": 0.0,
     "save_every": 80000//50,
     "print_every": 50,
@@ -65,11 +65,11 @@ config["model_config"] = {
     "d_conv": 4,
     "expand": 2,
     # diffusion config
-    "diffusion_batch": 512,
+    "diffusion_batch": 192,
     "layer_channels": [1, 32, 64, 128, 64, 32, 1],
     "dim_per_token": config["dim_per_token"],
     "kernel_size": 7,
-    "sample_mode": DDPMSampler,
+    "sample_mode": DDIMSampler,
     "beta": (0.0001, 0.02),
     "T": 1000,
     "forward_once": True,
